5a72f8fb565d4ac97b186336159647d3dd5f3703
Lucy Ofem <ofemlucy@gmail.com>
2020-05-11 14:48:28 +1300

    changed app.js

--- app.js
+++ app.js
@@ -1,52 +1,88 @@
 const express = require("express")
 const mysql = require("mysql")
-const bodyparser = require("body-parser")
 
 const app = express()
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
+
 
 const con = mysql.createConnection({
-  use: "cmsdb",
   host: "localhost",
-  user: "root",
-  database: "cmsdb"
+  user: "root"
 })
 
 con.connect(function (error){
-if(error){
-  console.log(error)
-}else {
-console.log("connected")
-}
-})
+  if(error){
+    console.log(error)
+  }else {
+  console.log("connected")
+  }
+  })
 
-const table1 = 'CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY,email VARCHAR(255),password INT(11),role ENUM("user","admin"),deleted_at TIMESTAMP,is_enabled TIMESTAMP)ENGINE=INNODB'
+const connect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "cmsdb"
+  })
 
-con.query(table1, function (error, result) {
+  connect.query("CREATE DATABASE IF NOT EXISTS cmsdb", function(error){
+    if(error) {
+      console.log(error.message)
+    }
+    console.log("database created!")
+  })
+
+  const table1 = 'CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,email VARCHAR(255),password VARCHAR(50),role ENUM("user","admin"),deleted_at TIMESTAMP,is_enabled TIMESTAMP)ENGINE=INNODB'
+
+connect.query(table1, function (error, result) {
   if (error) {
     console.log(error.message)
   } else
     console.log("table1 created")
 })
 
-const table2 = "CREATE TABLE project(id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, name VARCHAR(255), description TEXT, token VARCHAR(255), created_at TIMESTAMP, updated_at TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)ENGINE=INNODB"
+const sql1 = "INSERT INTO users(id,email,password,role,deleted_at, is_enabled) VALUES(NULL, 'lucy@gmail.com', 'dgu5vjhfuiy', 'user', 0.00, 0.00)"
 
-con.query(table2, function (error, result) {
+connect.query(sql1, function(error, result){
+  if(error) {
+    console.log(error)
+  }
+  console.log("inserted number of rows in table1:" + result.affectedRows)
+})
+
+const table2 = "CREATE TABLE IF NOT EXISTS project(id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, name VARCHAR(255), description TEXT, token VARCHAR(255), created_at TIMESTAMP, updated_at TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)ENGINE=INNODB"
+
+connect.query(table2, function (error, result) {
   if (error) {
     console.log(error.message)
   } else
     console.log("table2 created!")
 })
 
-const table3 = "CREATE TABLE post(id INT AUTO_INCREMENT PRIMARY KEY,user_id INT,project_id INT, title VARCHAR(100), body TEXT, published TIMESTAMP, created_at TIMESTAMP, updated_at TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY(project_id) REFERENCES project(id) ON DELETE CASCADE)ENGINE=INNODB"
+const sql2 = "INSERT INTO project(id,user_id,name,description,token,created_at,updated_at) VALUES(NULL, 1, 'mike', 'hdskuhfdbjkds', 'favfhdje56', 0.00,0.00)"
 
-con.query(table3, function (error, result) {
+connect.query(sql2, function(error,result){
+  if(error) {
+    console.log(error)
+  }
+  console.log("inserted number of rows in table2:" + result.affectedRows)
+})
+
+const table3 = "CREATE TABLE IF NOT EXISTS post(id INT AUTO_INCREMENT PRIMARY KEY,user_id INT,project_id INT, title VARCHAR(100), body TEXT, published TIMESTAMP, created_at TIMESTAMP, updated_at TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY(project_id) REFERENCES project(id) ON DELETE CASCADE)ENGINE=INNODB"
+
+connect.query(table3, function (error, result) {
   if (error) {
     console.log(error.message)
   } else {
     console.log("table3 created")
   }
+})
+
+const sql3 = "INSERT INTO post(id,user_id,project_id,title,body,published,created_at,updated_at) VALUES(NULL,1, 1, 'headlesscms', 'cdhgsjkhlkjl',0.00, 0.00,0.00)"
+
+connect.query(sql3, function(error,result){
+  if(error) {
+    console.log(error)
+  }
+  console.log("inserted number of rows in table3:" + result.affectedRows)
 })
 
 app.listen(8000, function () {
